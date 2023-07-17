d2caac9c3d9567fc355d6780d90b0b44cd716795
davidnvincent <davela1999@gmail.com>
2023-07-17 10:47:22 -0500

    account remediation
    Tracks users' attempts to perform account remediation for common social media websites. In collaboration with a few DU students. See README for steps.

---
+++ Account Remediation/files/Pinterest/main.js
@@ -0,0 +1,145 @@
+const regex = /[^A-Za-z0-9]/g;
+let csvContent = "data:text/csv;charset=utf-8,";
+
+let anchors = [];
+let clickedArr = [];
+let hoveredArr = [];
+let mouseCoords = []
+let clicks = 0;
+
+window.onload = function() {
+    
+	anchors = document.getElementsByTagName("a");
+	
+    // Tracks which anchor elements are clicked and/or hovered over //
+
+    for (let i = 0; i < anchors.length; i++) {
+        
+		// Element clicked //
+		
+		anchors[i].removeAttribute("href");
+		anchors[i].style.cursor = "pointer";
+        
+		anchors[i].onclick = function() {
+			onEvent(clickedArr, i);
+			clicks++;
+			alert("Demo Page.");
+		}
+		
+        // Mouse hovering element //
+        
+		anchors[i].onmouseover = function() {
+            onEvent(hoveredArr, i);
+        }; 
+		
+    };
+
+    // Tracks mouse moverment //
+	
+    document.addEventListener('mousemove', (e) => {
+        mouseCoords.push([e.pageX, e.pageY]);
+    });
+
+}
+
+function onEvent(arr, i) { 
+    let anchorName = "";
+            
+	//First checks whether the anchor has text within it
+            
+	if (anchors[i].text != null) {
+		anchorName = anchors[i].text;
+    }
+            
+	//Then checks whether the anchor has a title
+            
+	else if (anchors[i].title != null) {
+		anchorName = anchors[i].title;
+    }
+			
+	anchorName = anchorName.replace(regex, "");
+			
+	if (exists(arr, anchorName)) {
+		for (var k = 0; k < arr.length; k++) {
+			if(arr[k][0] == anchorName) {
+				arr[k][1]++;
+				break;
+			}
+		}
+	}
+			
+	else {
+		arr.push([anchorName, 1]);
+	}
+			
+	console.log(arr);
+			
+};
+
+function exists(arr, search) {
+    return arr.some(row => row.includes(search));
+}
+
+function mouseEventData() {
+	
+	if (Array.isArray(clickedArr) && clickedArr.length) {
+		csvContent += "Click Data\r\nTotal Clicks, " + clicks + "\r\n---\r\nLink Text, Times Clicked\r\n";
+		
+		clickedArr.forEach(function(rowArray) {
+			let row = rowArray.join(",");
+			csvContent += row + "\r\n";
+		});
+		
+		csvContent += "\r\n\r\n\r\n";
+		
+	}
+	
+	if (Array.isArray(hoveredArr) && hoveredArr.length) {
+		csvContent += "Hover Data\r\n---\r\nLink Text, Times Clicked\r\n";
+		
+		hoveredArr.forEach(function(rowArray) {
+			let row = rowArray.join(",");
+			csvContent += row + "\r\n";
+		});
+		
+	}
+	
+	var encodedUri = encodeURI(csvContent);
+	var link = document.createElement("a");
+	link.setAttribute("href", encodedUri);
+	link.setAttribute("download", "mouseEventData.csv");
+	document.body.appendChild(link); // Required for FF
+
+	link.click();
+	
+	csvContent = "data:text/csv;charset=utf-8,";
+	
+}
+
+function mousePositionData() {
+	csvContent += "X Coordinate, Y Coordinate\r\n";
+	
+	mouseCoords.forEach(function(rowArray) {
+		let row = rowArray.join(",");
+		csvContent += row + "\r\n";
+	});
+	
+	var encodedUri = encodeURI(csvContent);
+	var link = document.createElement("a");
+	link.setAttribute("href", encodedUri);
+	link.setAttribute("download", "mousePositionData.csv");
+	document.body.appendChild(link); // Required for FF
+
+	link.click();
+	
+	csvContent = "data:text/csv;charset=utf-8,";
+	
+}
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,21 @@
+Software Engineering Final Project Group 10
+
+HOW TO CREATE A SIMULATION PAGE:
+
+* Test user downloads their personal source code and send it to researcher​
+
+* Researcher uses the MHTML converter to produce the simulated webpage​
+     * [Mht2htm.exe​](https://sourceforge.net/projects/mht2htm/)
+
+* Link our JavaScript at the top of HTML​
+     * `<script src="main.js"></script>`​
+
+* Start the simulation with the test user​
+
+* Researcher can export desired data from within browser console​ once the simulation test has concluded
+     * mouseEventData() - Exports CSV of Click and Hover Data​
+     * mousePositionData() - Exports CSV of mouse position from the start of the simulation​
+
+* View data in spreadsheet viewer such as Excel for analysis​
+
+​
